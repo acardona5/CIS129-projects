54b6fbf084f18f580c915fc68d859fdead06cc35
acardona5 <acardona5@mail.pima.edu>
2024-09-09 23:45:07 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Auther: Amy Cardona
+# My first Python lab in my CIS129 course
+# Print a simple short story
+ print('Once upon a time...\nThere was a chicken gazing over a river. The river was wide and seemed too daunting to cross by\
+ flight. Yet over that river, the most enchanting tall glass covered medow glistening in the sun awaited. Whilst longing\
+to be on the other side of the river a tiny fairy with iridescent wings fluttering apeared above, manifested from the wishes\
+ of the chicken. In a blink of an eye the fairy turned the river into a solid road for the chicken to cross. This is how\
+the chicken crossed the road.\n The End.')
