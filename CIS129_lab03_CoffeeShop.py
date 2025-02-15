6d43fa71cb0065942f0ab57b9501fb2d396b7fde
acardona5 <acardona5@mail.pima.edu>
2025-02-14 23:05:14 -0700

    Update CIS129_lab03_CoffeeShop.py

--- CIS129_lab03_CoffeeShop.py
+++ CIS129_lab03_CoffeeShop.py
@@ -3,12 +3,12 @@
 # Lab 3: Coffe Shop
 #This program calculates the total cost of a coffee order
 #based on the number of items ordered and cost per item
-#updates add more menu items and print out new reciept based on items
 
 #variables
 muffin_price = 4
 coffee_price = 5
-chocolateChipCookie_price = 3
+chocolateChipCookie_price = 1
+icedTea_price = 3.50
 tax = 6   #percentage
 tax_rate = tax / 100
 stars = '*' * 35
@@ -19,12 +19,13 @@ print(stars)
 muffin_qty = int(input("Number of muffins bought?\n "))
 coffee_qty = int(input("Number of coffees bought?\n "))
 chocolateChipCookie_qty = int(input("Number of chocolate chip cookies bought?\n "))
+icedTea_qty = int(input("Number of iced teas bought?\n "))
 print(stars)
 
 print(stars)
 
 #processing
-subtotal = (muffin_qty * muffin_price) + (coffee_qty * coffee_price) + (chocolateChipCookie_qty * chocolateChipCookie_price)
+subtotal = (muffin_qty * muffin_price) + (coffee_qty * coffee_price) + (chocolateChipCookie_qty * chocolateChipCookie_price) + (icedTea_qty * icedTea_price)
 addedtax = subtotal * tax_rate
 total = subtotal
 total = total + addedtax
@@ -33,9 +34,11 @@ total = total + addedtax
 print(coffee_qty,'Coffees bought at $', coffee_price, 'each: $', float(coffee_qty * coffee_price))
 print(muffin_qty,'Muffins bought at $', muffin_price, 'each: $', float(muffin_qty * muffin_price))
 print(chocolateChipCookie_qty,'Chocolate Chip Cookies bought at $', chocolateChipCookie_price, 'each: $', float(chocolateChipCookie_qty * chocolateChipCookie_price))
+print(icedTea_qty,'Iced Teas bought at $', icedTea_price, 'each: $', float(icedTea_qty * icedTea_price))
+
 print('---------')
 print('Subtotal: $', float(subtotal))
-print(tax, '% Tax: $',addedtax)
+print(tax, '% Tax: $',float(addedtax))
 print('---------')
 print('Total: $', total)
 print('Enoy your sweet treats! ^0^')
